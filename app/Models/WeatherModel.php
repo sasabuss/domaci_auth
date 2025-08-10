85da38a2c07365c572e2615159a8f5103fc35692
Sasa <sasabuss96@gmail.com>
2025-08-10 18:35:35 +0200

    *Weather Model

---
+++ app/Models/WeatherModel.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class WeatherModel extends Model
+{
+    protected $table = 'weather';
+
+    protected $fillable = 'city';
+    protected $fillable = 'temperature';
+}
