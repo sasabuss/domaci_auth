278220c57a11e31b46acf03b057113d43893a48a
Sasa <sasabuss96@gmail.com>
2025-08-10 18:31:56 +0200

    *Starting Weather App

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,37 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\WeatherController;
 use Illuminate\Support\Facades\Route;
+
+
+
+
+Route::get("/home",function(){
+  return "Hello world";
+});
+
+Route::get("/about",function(){
+    return "About";
+});
+
+Route::get("/contact",function(){
+     return "Contact";
+});
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get("/prognoza",[WeatherController::class,'index']);
+
+
+
+
+
+
+
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
